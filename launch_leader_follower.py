14342f23f4cdf6bc5a7fdd08eb34ed867de011a6
hygradme <hygradme@gmail.com>
2023-12-03 17:06:07 +0900

    Initial commit

---
+++ launch_leader_follower.py
@@ -0,0 +1,64 @@
+import math
+import time
+from robot_controller import RobotContorller
+from xarm.wrapper import XArmAPI
+from lite6_leader import Lite6Leader
+from control_openRB150_with_modbus_rtu import ParalleGripperOpenRB150
+
+
+def move_to_start_position(arm, servo_initial):
+    arm.set_mode(mode=0)
+    arm.set_state(0)
+    time.sleep(0.2)
+
+    arm.set_servo_angle(angle=servo_initial[:6], is_radian=True, speed=0.3, wait=True)
+    time.sleep(0.2)
+    arm.set_mode(mode=1)
+    arm.set_state(0)
+    time.sleep(1)
+
+
+if __name__ == "__main__":
+    DEVICENAME = "COM3"
+    use_gripper = True
+    use_follower = True
+
+    ip_robot = "192.168.1.160"
+    arm = XArmAPI(ip_robot, is_radian=True)
+    DXL_IDs = [1, 2, 3, 4, 5, 6]
+    if use_gripper:
+        pga = ParalleGripperOpenRB150(arm)
+        gripper_id = 0
+        DXL_IDs.append(gripper_id)
+        gripper_scale = 4
+    leader = Lite6Leader(DEVICENAME, DXL_IDs=DXL_IDs)
+
+    DH_params = None
+    rc = RobotContorller(arm, DH_params, filter_size=5, filter_type=None)
+
+    # initialize
+    angles = leader.get_angles_from_leader(is_radian=True)
+    time.sleep(2)
+
+    if use_follower:
+        move_to_start_position(arm, angles[:6])
+    time.sleep(1)
+    print("start following")
+    if use_gripper:
+        # pga.change_goal_current_val(50)
+        time.sleep(0.5)
+
+    while True:
+        angles = leader.get_angles_from_leader(is_radian=True)
+        print([round(angle * 180 / math.pi, 1) for angle in angles])
+        if use_follower:
+            rc.move_robot_joint(angles[:6], is_radian=True)
+            if use_gripper:
+                gripper_pos = int(angles[6] * gripper_scale * 180 / math.pi)
+                gripper_pos = max(min(gripper_pos, 250), 0)
+                pga.move(gripper_pos)
+        else:
+            time.sleep(0.02)
+        # pos = pga.get_pos()
+        # print("position", pos)
+
---
+++ lite6_leader.py
@@ -0,0 +1,81 @@
+import time
+from dynamixel_sdk import *                    # Uses Dynamixel SDK library
+import math
+
+
+def convert_dxl_to_int32(dxl_value):
+    # 32ビット符号付き整数の最大値
+    INT32_MAX = 2147483647
+
+    # 値が32ビット符号付き整数の最大値より大きい場合、負の値として解釈
+    if dxl_value > INT32_MAX:
+        return dxl_value - 4294967296  # 2^32から引く
+    else:
+        return dxl_value  # そのままの値を使用
+
+class Lite6Leader:
+    def __init__(self, DEVICENAME, DXL_IDs):
+        # Control table address
+        self.ADDR_PRO_PRESENT_POSITION = 132               # DXLの現在位置アドレス
+
+        # Protocol version
+        PROTOCOL_VERSION = 2.0                       # Dynamixelのプロトコルバージョン
+
+        # Default setting
+        self.DXL_IDs = DXL_IDs#[1, 2, 3, 4, 5, 6, 7]                 # DXLのIDリスト
+        BAUDRATE = 57600                             # Dynamixelのボーレート
+        # DEVICENAME = 'COM3'                          # ポート名
+        TORQUE_ENABLE = 1                            # トルクをON
+        TORQUE_DISABLE = 0                           # トルクをOFF
+        DXL_MOVING_STATUS_THRESHOLD = 20             # DXLの移動ステータスのしきい値
+
+        self.angle_offsets = [180] * len(self.DXL_IDs)
+
+        # Initialize PortHandler instance
+        # Set the port path
+        self.portHandler = PortHandler(DEVICENAME)
+
+        # Initialize PacketHandler instance
+        # Set the protocol version
+        self.packetHandler = PacketHandler(PROTOCOL_VERSION)
+
+        # Open port
+        if self.portHandler.openPort():
+            print("Port opened successfully")
+        else:
+            print("Failed to open the port")
+            quit()
+
+        # Set port baudrate
+        if self.portHandler.setBaudRate(BAUDRATE):
+            print("Baudrate set successfully")
+        else:
+            print("Failed to set baudrate")
+            quit()
+
+    def get_angles_from_leader(self, is_radian=True):
+        angles = []
+
+        for i, dxl_id in enumerate(self.DXL_IDs):
+            # Read present position
+            dxl_present_position, dxl_comm_result, dxl_error = self.packetHandler.read4ByteTxRx(self.portHandler, dxl_id, self.ADDR_PRO_PRESENT_POSITION)
+            if dxl_comm_result != COMM_SUCCESS:
+                print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+            elif dxl_error != 0:
+                print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+
+            # Convert to degree (assuming the motor is using default settings)
+            angle = (convert_dxl_to_int32(dxl_present_position) / 4096) * 360 - self.angle_offsets[i]
+            if is_radian:
+                angle = angle * math.pi / 180
+            angles.append(angle)
+        return angles
+
+
+if __name__ == "__main__":
+    leader = Lite6Leader()
+
+    while True:
+        angles = leader.get_angles_from_leader()
+        print(angles)
+        time.sleep(0.02)  # 0.02秒待機
